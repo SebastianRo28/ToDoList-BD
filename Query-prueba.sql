43c93a47b816261e7594115301f1f744bdd1a981
SebastianRo28 <sebastian.arflorentino28@gmail.com>
2024-06-26 05:16:31 -0300

    primer commit

---
+++ Query-prueba.sql
@@ -0,0 +1,23 @@
+USE [ToDoListDB]
+GO
+
+/****** Object:  Table [dbo].[Tarea]    Script Date: 26/06/2024 05:15:31 ******/
+SET ANSI_NULLS ON 
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tarea](
+	[TaskId] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [varchar](50) NULL,
+	[Description] [varchar](250) NULL,
+	[IsCompleted] [varchar](1) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[TaskId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
